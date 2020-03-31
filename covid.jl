4f2ef6104efe2e7d80fedce6f24bd57e443850a4
Markus Boeck <e1634838@student.tuwien.ac.at>
2020-03-31 16:12:16 +0200

    03-30 base

--- covid.jl
+++ covid.jl
@@ -81,7 +81,7 @@ function new_infected_plot(start_date,x,y,L,k,x0)
     return p1, p2
 end
 
-function SIR_prediction(start_date,I,R,D, μ, months=6, save=false)
+function SIR_prediction(start_date,I,R,D,μ; months=6, save=false)
     x = Array(0:length(I)-1)
     r = fit_exact_IR_1(x,I,R)
     i0 = I[1]
@@ -95,10 +95,6 @@ function SIR_prediction(start_date,I,R,D, μ, months=6, save=false)
     pred_D(t) = μ * exact_R(t, t0, b, c, s0, i0)
     pred_G(t) = (1-μ) * exact_R(t, t0, b, c, s0, i0)
 
-    # x_max = length(x)
-    # while pred_I(x_max) > 1000
-    #     x_max += 1
-    # end
     x_max = length(x) + 30 * months
     current = Dates.format(start_date + Day(length(I)-1), "d-mm-YY")
 
@@ -138,7 +134,7 @@ function SIR_prediction(start_date,I,R,D, μ, months=6, save=false)
         savefig("Predictions/SIR_Prediction_$(current).png")
     end
 
-    return p
+    return p, pred_I, pred_R
 end
 
 function daily_prediction(Infected, Recovered, Dead, start_date=DateTime(2020,2,25), prob=0.95)
@@ -157,59 +153,45 @@ function daily_prediction(Infected, Recovered, Dead, start_date=DateTime(2020,2,
 end
 
 
-# 25.03. 15:00
-Infected =  [2,2,3,6,10,14,18,21,27,39,53,77,100,138,178,242,356,497,648,853,1007,1320,1633,1998,2373,2798,3219,3894,4892,5549]
-Recovered = [0,0,0,0, 0, 0, 0, 0, 2, 2, 2, 2,  2,  2,  4,  4,  4,  6,  6,  6,   6,   9,   9,   9,   9,   9,   9,   9,   9,   9]
-Dead =      [0,0,0,0, 0, 0, 0, 0, 0, 0, 0, 0,  0,  0,  0,  0,  1,  1,  1,  1,   3,   3,   4,   6,   6,   7,  16,  21,  25,  30]
-p, p1, base = daily_prediction(Infected, Recovered, Dead)
-display(p1)
 
-# 26.03 15:00
-#Dates =    [25.2.26.2,27.2,28.2,29.2,01.3,02.3,03.3,04.3,05.3,06.3,07.3,08.3,09.3,10.3,11.3,12.3,13.3,14.3,15.3,16.3,17.3,18.3,19.3,20.3,21.3,22.3,23.3,24.3,25.3.26.3,27.3,28.3,29.3]
-Infected =  [   2,   2,   3,   6,  10,  14,  18,  21,  27,  39,  53,  77, 100, 138, 178, 242, 356, 497, 648, 853,1007,1320,1633,1998,2373,2793,3214,3889,4887,5544,6335,7158,7552,7971]
-Recovered = [   0,   0,   0,   0,   0,   0,   0,   0,   2,   2,   2,   2,   2,   2,   4,   4,   4,   6,   6,   6,   6,   9,   9,   9,   9,   14,   14,  14,  14,   14,  14, 225, 410, 479]
-Dead =      [   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   1,   1,   1,   1,   3,   3,   4,   6,   6,   7,  16,  21,  25,  30,  49,  58,  68,  86]
-p, p1, base = daily_prediction(Infected, Recovered, Dead)
-display(p1)
+function smooth_recovered(Recovered, Dead, until=nothing)
+    if until == nothing
+        weights = Dead ./ sum(Dead)
 
-scatter(Recovered)
-
-function smooth_recovered(Recovered, Dead)
-    weights = Dead ./ sum(Dead)
-
-    return sum(Recovered) .* weights
+        return sum(Recovered) .* weights
+    else
+        weights = Dead[1:until] ./ sum(Dead[1:until])
+        r = copy(Recovered)
+        r[1:until] = r[1:until] .* weights
+        return r
+    end
 end
 
-scatter(smooth_recovered(Recovered, Dead))
 
 function estimate_μ(Recovered,Dead)
     return sum(Dead) / (sum(Dead) + sum(Recovered))
 end
 
+# BASELINE 30.3.
+
+# February
+# Dates =   [25.2.26.2,27.2,28.2,29.2]]
+Infected =  [   2,   2,   3,   6,  10]
+Recovered = [   0,   0,   0,   0,   0]
+Dead =      [   0,   0,   0,   0,   0]
+
+# March
+# Dates =       [01.3,02.3,03.3,04.3,05.3,06.3,07.3,08.3,09.3,10.3,11.3,12.3,13.3,14.3,15.3,16.3,17.3,18.3,19.3,20.3,21.3,22.3,23.3,24.3,25.3.26.3,27.3,28.3,29.3,30.3]
+push!(Infected, [  14,  18,  21,  27,  39,  53,  77, 100, 138, 178, 242, 356, 497, 648, 853,1007,1320,1633,1998,2373,2793,3212,3875,4833,5490,6237,7158,7552,7971,8633]...)
+push!(Recovered,[   0,   0,   0,   2,   2,   2,   2,   2,   2,   4,   4,   4,   6,   6,   6,   6,   9,   9,   9,   9,  14,  16,  28,  68,  68, 112, 225, 410, 479, 636]...)
+push!(Dead,     [   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   1,   1,   1,   1,   3,   3,   4,   6,   6,   7,  16,  21,  25,  30,  49,  58,  68,  86, 108]...)
+
+
 I = Infected
-R = smooth_recovered(Recovered, Dead) .+ Dead
+R = Recovered .+ Dead
 D = Dead
 μ = estimate_μ(Recovered, Dead)
 
-SIR_prediction(Date(2020,2,25),I,R,D,μ)
+p, pred_I, pred_R = SIR_prediction(Date(2020,2,25),I,R,D,μ,save=true)
 
-
-x = Array(0:length(I)-1)
-r = fit_exact_IR_1(x,I,R)
-i0 = I[1]
-(s0, t0, γ, R0) = r.minimizer
-
-b = γ * R0 / (R0 - 1)
-c = γ / (R0 - 1)
-
-scatter(x, I)
-plot!(t -> exact_I(t, t0, b, c, s0, i0))
-
-scatter(x, R)
-plot!(t -> exact_R(t, t0, b, c, s0, i0))
-
-
-plot(t -> exact_I(t, t0, b, c, s0, i0),0,200)
-plot!(t -> exact_R(t, t0, b, c, s0, i0),0,200)
-scatter!(x, I)
-scatter!(x, R)
+display(p)
